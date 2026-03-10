a6c974a3142e63f3b9b7497549a1ccc494f911a3
HaziqRazali <ha_haziq_91@hotmail.com>
2026-03-10 22:56:34 +0800

    sync

--- run_IROS_2026.sh
+++ run_IROS_2026.sh
@@ -10,9 +10,6 @@ python scripts/smplx_to_robot.py --smplx_file /home/haziq/datasets/mocap/data/hu
 python scripts/smplx_to_robot.py --smplx_file /home/haziq/datasets/mocap/data/humaneva/train/S1/smplx/Jog_1.npz --robot tienkung --save_path motion.npz --rate_limit
 python scripts/smplx_to_robot.py --smplx_file /home/haziq/datasets/mocap/data/humaneva/train/S1/smplx/Jog_1.npz --robot openloong --save_path motion.npz --rate_limit
 
-python3 scripts/generate_smplx_retarget.py assets/unitree_g1/g1_custom_collision_29dof.urdf --human-scale 0.9 --arm-scale 0.8 --ground-height 0.0 --no-head -o output4.json
-python3 scripts/generate_smplx_retarget.py assets/tienkung/urdf/tienkung2_lite.urdf --human-scale 0.9 --arm-scale 0.8 --ground-height 0.0 --no-head -o /home/haziq/GMR/general_motion_retargeting/ik_configs/smplx_to_tienkung.json
-
 # # # # # # #
 # h1        #
 # # # # # # #
@@ -27,29 +24,44 @@ python3 scripts/generate_smplx_retarget.py \
 
 python scripts/smplx_to_robot.py \
   --smplx_file /home/haziq/datasets/mocap/data/fit3d/train/s04/smplx/warmup_9.json \
-  --robot unitree_h1 --rate_limit --record_video --camera_distance 5.5 --camera_elevation -15 --rotate_yaw -90 \
-  --save_path unitree_h1_warmup_9_generated.npz \
-  --video_path videos/unitree_h1_warmup_9_generated.mp4 \
-  --ik_config general_motion_retargeting/ik_configs/smplx_to_h1.json
-  
-python scripts/smplx_to_robot.py \
-  --smplx_file /home/haziq/datasets/mocap/data/fit3d/train/s04/smplx/warmup_9.json \
-  --robot unitree_h1 --rate_limit --record_video --camera_distance 5.5 --camera_elevation -15 --rotate_yaw -90 \
+  --robot unitree_h1 --rate_limit --record_video --camera_distance 5.5 --camera_elevation -15 --camera_height 1 --rotate_yaw -90 \
   --save_path unitree_h1_warmup_9_generated.npz \
   --video_path videos/unitree_h1_warmup_9_generated.mp4 \
   --ik_config general_motion_retargeting/ik_configs/smplx_to_h1_generated.json
+  
+python scripts/smplx_to_robot.py \
+  --smplx_file /home/haziq/datasets/mocap/data/fit3d/train/s04/smplx/warmup_9.json \
+  --robot unitree_h1 --rate_limit --record_video --camera_distance 5.5 --camera_elevation -15 --camera_height 1 --rotate_yaw -90 \
+  --save_path unitree_h1_warmup_9_true.npz \
+  --video_path videos/unitree_h1_warmup_9_true.mp4 \
+  --ik_config general_motion_retargeting/ik_configs/smplx_to_h1.json
+
+# bicep curl
+python scripts/smplx_to_robot.py \
+  --smplx_file /home/haziq/datasets/mocap/data/fit3d/train/s04/smplx/dumbbell_biceps_curls.json \
+  --robot unitree_h1 --rate_limit --record_video --camera_distance 5.5 --camera_elevation -15 --camera_height 1 --rotate_yaw -90 \
+  --save_path unitree_h1_bicep_curl_generated.npz \
+  --video_path videos/unitree_h1_bicep_curl_generated.mp4 \
+  --ik_config general_motion_retargeting/ik_configs/smplx_to_h1_generated.json
+  
+python scripts/smplx_to_robot.py \
+  --smplx_file /home/haziq/datasets/mocap/data/fit3d/train/s04/smplx/dumbbell_biceps_curls.json \
+  --robot unitree_h1 --rate_limit --record_video --camera_distance 5.5 --camera_elevation -15 --camera_height 1 --rotate_yaw -90 \
+  --save_path unitree_h1_bicep_curl_true.npz \
+  --video_path videos/unitree_h1_bicep_curl_true.mp4 \
+  --ik_config general_motion_retargeting/ik_configs/smplx_to_h1.json
 
 # # # # # # #
 # openloong #
 # # # # # # #
 
 python3 scripts/generate_smplx_retarget.py \
-  assets/openloong/AzureLoong.urdf \
-  --human-scale 0.9 \
-  --arm-scale 0.8 \
-  --ground-height 0.0 \
-  --no-head \
-  -o general_motion_retargeting/ik_configs/smplx_to_openloong_generated.json
+assets/openloong/AzureLoong.urdf \
+--human-scale 0.9 \
+--arm-scale 0.8 \
+--ground-height 0.0 \
+--no-head \
+-o general_motion_retargeting/ik_configs/smplx_to_openloong_generated.json
 
 python scripts/smplx_to_robot.py \
   --smplx_file /home/haziq/datasets/mocap/data/fit3d/train/s04/smplx/warmup_9.json \
@@ -61,13 +73,24 @@ python scripts/smplx_to_robot.py \
 python scripts/smplx_to_robot.py \
   --smplx_file /home/haziq/datasets/mocap/data/fit3d/train/s04/smplx/warmup_9.json \
   --robot openloong --rate_limit --record_video --camera_distance 5.5 --camera_elevation -15 --rotate_yaw -90 \
-  --save_path openloong_warmup_9_generated.npz \
-  --video_path videos/openloong_warmup_9_generated.mp4 \
+  --save_path openloong_warmup_9_true.npz \
+  --video_path videos/openloong_warmup_9_true.mp4 \
   --ik_config general_motion_retargeting/ik_configs/smplx_to_openloong.json
 
 # # # # # # #
 # tienkung  #
 # # # # # # #
+
+python3 scripts/generate_smplx_retarget.py \
+  assets/tienkung/urdf/tienkung2_lite.urdf \
+  --robot-root Base_link \
+  --human-scale 1.0 \
+  --arm-scale 1.0 \
+  --ground-height -0.05 \
+  --profile default \
+  --approx-arms \
+  --no-hip-anatomy \
+  -o general_motion_retargeting/ik_configs/smplx_to_tienkung_generated.json
 
 python scripts/smplx_to_robot.py \
   --smplx_file /home/haziq/datasets/mocap/data/fit3d/train/s04/smplx/warmup_9.json \
@@ -146,6 +169,14 @@ python scripts/smplx_to_robot.py \
 # # # # # #
 # unitree #
 # # # # # #
+
+python3 scripts/generate_smplx_retarget.py \
+assets/unitree_g1/g1_custom_collision_29dof.urdf \
+--human-scale 0.9 \
+--arm-scale 0.8 \
+--ground-height 0.0 \
+--no-head \
+-o general_motion_retargeting/ik_configs/smplx_to_g1_generated.json
 
 # warmup_9
 python scripts/smplx_to_robot.py \
@@ -237,3 +268,6 @@ python scripts/export_robot_motion_to_blender.py --motion_file objidx4_repidx0.n
 
 python scripts/smplx_to_robot_v2.py idx=212 result_foldername="/home/haziq/Collab_AI/results/AAAI2026/2024_11_18/gimo/obj2pose/cvae=all_v2_transl=mse_orient=mse_pred_all_objects_dtype=val_seminar_room1_0221_2022-02-21-014246_255_mib/" obj_idx=6 rep_idx=0 --robot unitree_g1 --save_path objidx6_repidx0.npz --rate_limit
 python scripts/export_robot_motion_to_blender.py --motion_file objidx6_repidx0.npz --robot unitree_g1 --output blender_export/objidx6_repidx0/robot_motion.fbx
+
+python scripts/smplx_to_robot_v2.py idx=215 result_foldername="/home/haziq/Collab_AI/results/AAAI2026/2024_11_18/gimo/obj2pose/cvae=all_v2_transl=mse_orient=mse_pred_all_objects_dtype=val_seminar_room1_0221_2022-02-21-014246_mib/" obj_idx=6 rep_idx=0 --robot unitree_g1 --save_path objidxxxx_repidx0.npz --rate_limit
+python scripts/export_robot_motion_to_blender.py --motion_file objidxxxx_repidx0.npz --robot unitree_g1 --output blender_export/objidxxxx_repidx0/robot_motion.fbx
